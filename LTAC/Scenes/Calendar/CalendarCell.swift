164ce66f02a865c05658355466c238e5f914c48e
Artem <kohanartem@gmail.com>
2023-11-15 18:34:20 +0300

    Почему то не попало в коммит

--- LTAC/Scenes/Calendar/CalendarCell.swift
+++ LTAC/Scenes/Calendar/CalendarCell.swift
@@ -12,19 +12,37 @@ final class CalendarCell: UICollectionViewCell {
     
     var currentEvent: CalendarEventModel?
     
+    let dateFormatter = DateFormatter()
+    
+    private lazy var mainContentView: UIView = {
+        let view = UIView()
+        view.layer.cornerRadius = 12
+        view.backgroundColor = .ltacWhiteAccent
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
     private var eventNumberLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.textColor = .ltacBlack
-        label.font = .boldSystemFont(ofSize: 12)
+        label.font = .boldSystemFont(ofSize: 27)
         return label
     }()
     
-    private var eventDateLabel: UILabel = {
+    private var eventDayLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.textColor = .ltacBlack
-        label.font = .boldSystemFont(ofSize: 12)
+        label.font = .systemFont(ofSize: 30)
+        return label
+    }()
+    
+    private var eventMonthLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .ltacBlack
+        label.font = .systemFont(ofSize: 18)
         return label
     }()
     
@@ -38,65 +56,78 @@ final class CalendarCell: UICollectionViewCell {
     
     private var trackLogoImage: UIImageView = {
         let image = UIImageView()
-        image.contentMode = .scaleAspectFill
+        image.contentMode = .scaleAspectFit
         image.translatesAutoresizingMaskIntoConstraints = false
         image.layer.cornerRadius = 12
         image.layer.masksToBounds = true
-
+        
         return image
     }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-
+        
         setupView()
         setupHierarchy()
         setupLayout()
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("Cannot initiate cell")
     }
-
+    
     private func setupView() {
-        backgroundColor = .ltacWhiteAccent
+        backgroundColor = .ltacWhiteBase
     }
-
+    
     func setupHierarchy() {
-        contentView.addSubview(eventNumberLabel)
-        contentView.addSubview(eventDateLabel)
-        contentView.addSubview(trackLogoImage)
-        contentView.addSubview(configurationLabel)
+        mainContentView.addSubview(eventNumberLabel)
+        mainContentView.addSubview(eventDayLabel)
+        mainContentView.addSubview(eventMonthLabel)
+        mainContentView.addSubview(trackLogoImage)
+        mainContentView.addSubview(configurationLabel)
+        addSubview(mainContentView)
     }
-
+    
     func setupLayout() {
         NSLayoutConstraint.activate([
-            eventNumberLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
-            eventNumberLabel.topAnchor.constraint(equalTo: contentView.topAnchor),
-            eventNumberLabel.heightAnchor.constraint(equalToConstant: 80),
-            eventNumberLabel.widthAnchor.constraint(equalToConstant: 80),
-
-            eventDateLabel.trailingAnchor.constraint(equalTo: eventNumberLabel.trailingAnchor, constant: 6),
-            eventDateLabel.topAnchor.constraint(equalTo: eventNumberLabel.topAnchor, constant: -6),
-            eventDateLabel.heightAnchor.constraint(equalToConstant: 42),
-            eventDateLabel.widthAnchor.constraint(equalToConstant: 42),
-
-            trackLogoImage.leadingAnchor.constraint(equalTo: eventNumberLabel.trailingAnchor, constant: 12),
-            trackLogoImage.topAnchor.constraint(equalTo: eventNumberLabel.topAnchor, constant: 7),
-            trackLogoImage.heightAnchor.constraint(equalToConstant: 22),
-
-            configurationLabel.leadingAnchor.constraint(equalTo: eventNumberLabel.trailingAnchor, constant: 12),
-            configurationLabel.topAnchor.constraint(equalTo: eventNumberLabel.bottomAnchor, constant: 4),
-            configurationLabel.heightAnchor.constraint(equalToConstant: 12)
+            mainContentView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            mainContentView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            mainContentView.topAnchor.constraint(equalTo: topAnchor),
+            mainContentView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            
+            eventNumberLabel.leadingAnchor.constraint(equalTo: mainContentView.leadingAnchor, constant: 12),
+            eventNumberLabel.centerYAnchor.constraint(equalTo: mainContentView.centerYAnchor),
+            
+            eventDayLabel.leadingAnchor.constraint(equalTo: eventNumberLabel.trailingAnchor, constant: 40),
+            eventDayLabel.topAnchor.constraint(equalTo: mainContentView.topAnchor, constant: 20),
+            
+            eventMonthLabel.centerXAnchor.constraint(equalTo: eventDayLabel.centerXAnchor),
+            eventMonthLabel.topAnchor.constraint(equalTo: eventDayLabel.bottomAnchor, constant: 6),
+            
+            trackLogoImage.leadingAnchor.constraint(equalTo: eventDayLabel.trailingAnchor, constant: 20),
+            trackLogoImage.trailingAnchor.constraint(equalTo: mainContentView.trailingAnchor, constant: -6),
+            trackLogoImage.topAnchor.constraint(equalTo: mainContentView.topAnchor, constant: 15),
+            trackLogoImage.heightAnchor.constraint(equalToConstant: 40),
+            
+            configurationLabel.centerXAnchor.constraint(equalTo: trackLogoImage.centerXAnchor),
+            configurationLabel.topAnchor.constraint(equalTo: trackLogoImage.bottomAnchor, constant: 10),
+            
         ])
     }
-
+    
     func configure() {
         guard let currentEvent = currentEvent else { return }
         
         eventNumberLabel.text = "\(currentEvent.eventNumber)"
         
-        eventDateLabel.text = "\(currentEvent.eventDate)"
+        dateFormatter.dateFormat = "dd"
+        
+        eventDayLabel.text = dateFormatter.string(from: currentEvent.eventDate)
+        
+        dateFormatter.dateFormat = "MMMM"
+        
+        eventMonthLabel.text = dateFormatter.string(from: currentEvent.eventDate)
         
         trackLogoImage.image = currentEvent.eventTrack.trackLogo
         
